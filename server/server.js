cd128b73de8d6583a422d69f2766b1d36d9ee2af
stepanenkovladyslav <stepanenkovladyslav@gmail.com>
2023-09-04 23:32:09 +0300

    Added different route names

--- server/server.js
+++ server/server.js
@@ -5,13 +5,15 @@ const hbs = require('hbs')
 const cors = require('cors')
 
 const app = express()
+
 app.set('view engine', 'hbs')
+
 app.use(cors())
 app.use('/views' , express.static((__dirname, 'views')))
 app.use(express.urlencoded({extended: true}))
 
-app.post('/api/v1/', TodoController.createTask)
-app.put('/api/v1/complete', TodoController.completeTask)
-app.delete('/api/v1/', TodoController.deleteTask)
+app.post('/api/v1/tasks', TodoController.createTask)
+app.put('/api/v1/tasks/complete', TodoController.completeTask)
+app.delete('/api/v1/tasks', TodoController.deleteTask)
 
 app.listen(3000, () => console.log('started listening'))
