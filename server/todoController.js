20d216168e0595c4a8af9a7e5b28730b2c9aa479
stepanenkovladyslav <stepanenkovladyslav@gmail.com>
2023-09-05 15:56:06 +0300

    Made code cleaner

--- server/todoController.js
+++ server/todoController.js
@@ -1,60 +1,93 @@
 const fs = require('fs')
+const path = require('path')
+
+const todoFilePath = path.join(__dirname,  'todo.json');
 
 class TodoController {
 
-  static async createTask (req, res)  {
-    fs.readFile(__dirname + '/todo.json', (err, data) => {
-        const taskList = JSON.parse(data);
-        const id = taskList.tasks.length > 0 ? taskList.tasks.length + 1 : 1;
-        const task = {id: id,  task: req.body.task, status: "incomplete"}
-        taskList.tasks.push(task)
-        const newTaskList = JSON.stringify(taskList)
-        fs.writeFile(__dirname + '/todo.json', newTaskList, (err) => {
-          if (err) {
-            console.log(err)
-          }
-          console.log('File is written successfully')
-        })
-        res.render('add-task.hbs', {task: req.body.task, id:id })
-      })
-  }
+  static createTask (req, res)  {
+    fs.readFile(todoFilePath, (err, data) => {
+      if (err) {
+        return res.status(500).json({message: 'Internal Server Error'})
+      }
 
-  static async completeTask (req, res)  {
-    fs.readFile(__dirname + '/todo.json', (err, data) => {
-        const id = +req.body.id;
-        const taskList = JSON.parse(data);
-        const changedTasks = taskList.tasks.map(task => {
-          if (task.id === id) {
-            return {...task, status : 'completed'}
-          }
-            return task 
-        })
-        taskList.tasks = changedTasks;
-        fs.writeFile(__dirname + '/todo.json', JSON.stringify(taskList), (err) => {
-          if (err) {
-            console.log('error completing')
-          }
-          console.log('completed successfully')
-        })
+      const taskList = JSON.parse(data);
+      const id = taskList.tasks.length > 0 ? taskList.tasks.length + 1 : 1;
+      const task = {id: id,  task: req.body.task, status: "incomplete"}
+
+      taskList.tasks.push(task)
+      
+      fs.writeFile(path.join(__dirname, '/todo.json'), JSON.stringify(taskList), (err) => {
+        if (err) {
+          return res.status(500).json({message: 'Internal Server Error'})
+        }
+      })
+
+      return res.status(201).render('add-task.hbs', {task: req.body.task, id:id })
     })
   }
 
-  static async deleteTask (req, res)  {
-    fs.readFile(__dirname + '/todo.json', (err, data) => {
-        const id = +req.params.id;
-        const taskList = JSON.parse(data);
-        const neededTask = taskList.tasks.find(task => task.id === id)
-        if (!neededTask) {
-          return res.status(404).json({message: 'Task not found'})
+  static completeTask (req, res)  {
+    fs.readFile(todoFilePath, (err, data) => {
+      if (err) {
+       return res.status(500).json({message: 'Internal Server Error'})
+      }
+
+      const id = +req.body.id;
+      const taskList = JSON.parse(data);
+      const neededTask = taskList.tasks.find(task => task.id === id)
+
+      if (!neededTask) {
+        return res.status(404).json({message: "Task not found"})
+      }
+
+      if (neededTask.status === 'completed') {
+        return res.status(200).json(neededTask)
+      }
+
+      const changedTasks = taskList.tasks.map(task => {
+        if (task.id === id) {
+          return {...task, status : 'completed'}
         }
-        const newTaskList = taskList.tasks.filter(task => task.id !== id)          
-        taskList.tasks = newTaskList;
-        fs.writeFile(__dirname + '/todo.json', JSON.stringify(taskList), (err) => {
-          if (err) {
-            console.log('error completing')
-          }
-          res.status(204).end()
-        })
+          return task 
+      })
+
+      taskList.tasks = changedTasks;
+
+      fs.writeFile(path.join(__dirname, '/todo.json'), JSON.stringify(taskList), (err) => {
+        if (err) {
+          return res.status(500).json({message: 'Internal Server Error'})
+        }
+      })
+
+       return res.status(200).json({...neededTask, status: "completed"})
+    })
+  }
+
+  static deleteTask (req, res)  {
+    fs.readFile(todoFilePath, (err, data) => {
+      if (err) {
+        return res.status(500).json({message: 'Internal Server Error'})
+      }
+      
+      const id = +req.params.id;
+      const taskList = JSON.parse(data);
+      const neededTask = taskList.tasks.find(task => task.id === id)
+
+      if (!neededTask) {
+        return res.status(404).json({message: 'Task not found'})
+      }
+
+      const newTaskList = taskList.tasks.filter(task => task.id !== id)          
+      taskList.tasks = newTaskList;
+
+      fs.writeFile(path.join(__dirname, '/todo.json'), JSON.stringify(taskList), (err) => {
+        if (err) {
+          return res.status(500).json({message: 'Internal Server Error'})
+        }
+
+        return res.status(200).end('')
+      })
     })
   }
 
