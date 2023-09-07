c62f60a972d1da17f0f737468abed46349e9abc1
stepanenkovladyslav <stepanenkovladyslav@gmail.com>
2023-09-07 14:36:50 +0300

    Dealt with sending tasks to client + changeTaskStatus

--- server/server.js
+++ server/server.js
@@ -12,8 +12,9 @@ app.use(cors())
 app.use('/views' , express.static((__dirname, 'views')))
 app.use(express.urlencoded({extended: true}))
 
+app.get('/api/v1/tasks', TodoController.getTasks)
 app.post('/api/v1/tasks', TodoController.createTask)
-app.put('/api/v1/tasks/complete', TodoController.completeTask)
+app.put('/api/v1/tasks/complete', TodoController.changeTaskStatus)
 app.delete('/api/v1/tasks/:id', TodoController.deleteTask)
 
 app.listen(3000, () => console.log('started listening'))
--- server/todoController.js
+++ server/todoController.js
@@ -1,9 +1,38 @@
-const fs = require('fs')
+const fs = require('fs');
+const { handlebars } = require('hbs');
+const engine = require('engine-handlebars')(handlebars)
 const path = require('path')
 
 const todoFilePath = path.join(__dirname,  'todo.json');
+const templateFilePath = path.join(__dirname, 'views', 'add-task.hbs')
+
 
 class TodoController {
+
+  static getTasks(req, res) {
+    fs.readFile(todoFilePath, (err, data) => {
+      if (err) {
+        return res.status(500).json({message: 'Internal Server Error'})
+      }
+
+      const template = fs.readFileSync(templateFilePath, 'utf8');
+      const compileTasks = handlebars.compile(template);
+
+      const taskList = JSON.parse(data);
+
+      if (taskList.tasks.length > 0) {
+        const renderedTasks = taskList.tasks.map(item => {
+          if(item.status === 'completed') {
+            return compileTasks({task: item.task, id: item.id, status: 'checked'}) 
+          }
+          return compileTasks({task: item.task, id: item.id})
+        }) 
+        const combinedHTML = renderedTasks.join('')
+        return res.send(combinedHTML)
+      }
+      return res.status(204).send('')
+    })
+  }
 
   static createTask (req, res)  {
     fs.readFile(todoFilePath, (err, data) => {
@@ -26,7 +55,7 @@ class TodoController {
     })
   }
 
-  static completeTask (req, res)  {
+  static changeTaskStatus(req, res)  {
     fs.readFile(todoFilePath, (err, data) => {
       if (err) {
        return res.status(500).json({message: 'Internal Server Error'})
@@ -40,24 +69,22 @@ class TodoController {
         return res.status(404).json({message: "Task not found"})
       }
 
-      if (neededTask.status === 'completed') {
-        return res.status(200).json(neededTask)
-      }
+      const newStatus = neededTask.status === 'completed' ? 'incomplete' : 'completed'
 
       const changedTasks = taskList.tasks.map(task => {
         if (task.id === id) {
-          return {...task, status : 'completed'}
+          return {...task, status : newStatus}
         }
           return task 
       })
 
       taskList.tasks = changedTasks;
 
-      fs.writeFile(path.join(__dirname, '/todo.json'), JSON.stringify(taskList), (err) => {
+      fs.writeFile(todoFilePath, JSON.stringify(taskList), (err) => {
         if (err) {
           return res.status(500).json({message: 'Internal Server Error'})
         }
-       return res.status(200).json({...neededTask, status: "completed"})
+       return res.status(200).json({...neededTask, status: newStatus})
       })
     })
   }
